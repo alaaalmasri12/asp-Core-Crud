ecad9d41f1d49a6ac06b8eaea0e3b045ba763354
alaaalmasri12 <alaaa_almasri1997@yahoo.com>
2021-09-15 13:04:23 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mo3skarTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mo3skarTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<Employee> employeeList = new List<Employee>() {
+            new Employee
+            { ID=1,EmpName="Alaa",EmailAddress="alaaalmasri272@gmail.com"
+                ,BOD=new DateTime(2021, 9, 13),
+                Country ="Jordan",Salary=300,isActive=true},
+             new Employee
+            { ID=2,EmpName="ahmad",EmailAddress="ahmad@gmail.com"
+                ,BOD=new DateTime(2021, 8, 13),
+                Country ="Jordan",Salary=500,isActive=true},
+               new Employee
+            { ID=3,EmpName="khaled",EmailAddress="khaled@gmail.com"
+                ,BOD=new DateTime(2021, 8, 15),
+                Country ="Jordan",Salary=100,isActive=true}
+
+        };
+        public IActionResult AllEmployees()
+        {
+
+            return View(employeeList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            employeeList.Add(employee);
+            return RedirectToAction("AllEmployees");
+        }
+
+        public IActionResult Details(int ID)
+        {
+            var EmpDetails = (from item in employeeList where item.ID == ID select item).FirstOrDefault();
+
+            return View(EmpDetails);
+        }
+        [HttpGet]
+        public IActionResult Delete(int ID)
+        {
+            var EmpDetails = (from item in employeeList where item.ID == ID select item).FirstOrDefault();
+
+            return View(EmpDetails);
+        }
+        [HttpPost]
+        public IActionResult Delete(Employee employee)
+        {
+            if (employeeList.Count<=1)
+            {
+                employeeList.RemoveAt(0);
+                return RedirectToAction("AllEmployees");
+            }
+            else
+            {
+                employeeList.RemoveAt(employee.ID);
+                return RedirectToAction("AllEmployees");
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mo3skarTask.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string EmpName { get; set; }
+        public string EmailAddress{ get; set; }
+        public decimal Salary { get; set; }
+        public DateTime BOD { get; set; }
+        public  string Country { get; set; }
+        public bool isActive { get; set; }
+
+    }
+}
